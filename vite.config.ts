ad9f0eeb2f3cce1948f939b88137a021f5df0cbc
kevinescritor-hash <kevinescritor@gmail.com>
2026-02-05 22:26:59 +0100

    Ahora!

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CV/', // Explicitly set the base to the repository name for GitHub Pages
+  base: '/CV/', // Ruta base para GitHub Pages
   build: {
     outDir: 'dist',
+    emptyOutDir: true, // Limpia la carpeta dist antes de construir
     rollupOptions: {
       input: {
         main: './index.html'
