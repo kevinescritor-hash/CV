5f7f6938a08e2bf4836d55805276f4bbc6942b59
kevinescritor-hash <kevinescritor@gmail.com>
2026-02-05 22:24:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  base: '/CV/', // Explicitly set the base to the repository name for GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
